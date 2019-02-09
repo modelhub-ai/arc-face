73e619a176ea4cfd981f463fd0d601a1a475d8c1
Ahmed Hosny <i.ahmedhosny@gmail.com>
2019-02-09 13:24:06 -0500

    adds files

--- contrib_src/inference.py
+++ contrib_src/inference.py
@@ -1,8 +1,9 @@
-import onnx
-import caffe2.python.onnx.backend
 import json
 from processing import ImageProcessor
 from modelhublib.model import ModelBase
+import mxnet as mx
+import numpy as np
+from mxnet.contrib.onnx.onnx2mx.import_model import import_model
 
 
 class Model(ModelBase):
@@ -12,17 +13,26 @@ class Model(ModelBase):
         config = json.load(open("model/config.json"))
         # get the image processor
         self._imageProcessor = ImageProcessor(config)
-        # load the DL model (change this if you are not using ONNX)
-        self._model = onnx.load('model/model.onnx')
-    
+        # get context - cpu
+        ctx = mx.cpu()
+        image_size = (112,112)
+        # Import ONNX model
+        sym, arg_params, aux_params = import_model('model/model.onnx')
+        # Define and binds parameters to the network
+        model = mx.mod.Module(symbol=sym, context=ctx, label_names = None)
+        model.bind(data_shapes=[('data', (1, 3, image_size[0], image_size[1]))])
+        model.set_params(arg_params, aux_params)
+        self._model = model
 
     def infer(self, input):
         # load preprocessed input
         inputAsNpArr = self._imageProcessor.loadAndPreprocess(input)
-        # Run inference with caffe2 (change this if you are using a different DL framework)
-        results = caffe2.python.onnx.backend.run_model(self._model, [inputAsNpArr])
+        # Run inference with mxnet
+        input_blob = np.expand_dims(inputAsNpArr, axis=0)
+        data = mx.nd.array(input_blob)
+        db = mx.io.DataBatch(data=(data,))
+        self._model.forward(db, is_train=False)
+        embedding = self._model.get_outputs()[0].asnumpy()
         # postprocess results into output
-        output = self._imageProcessor.computeOutput(results)
-        return output
-        
-
+        output = self._imageProcessor.computeOutput(embedding)
+        return output.tolist()
--- contrib_src/processing.py
+++ contrib_src/processing.py
@@ -1,31 +1,115 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import cv2
+import numpy as np
+import os
+import mxnet as mx
+import sklearn
+from sklearn.preprocessing import normalize
+from mtcnn_detector import MtcnnDetector
+from skimage import transform as trans
 from modelhublib.processor import ImageProcessorBase
 import PIL
 import SimpleITK
 import numpy as np
 import json
 
-
 class ImageProcessor(ImageProcessorBase):
 
-    # OPTIONAL: Use this method to preprocess images using the image objects
-    #           they've been loaded into automatically.
-    #           You can skip this and just perform the preprocessing after
-    #           the input image has been convertet to a numpy array (see below).
     def _preprocessBeforeConversionToNumpy(self, image):
         if isinstance(image, PIL.Image.Image):
-            # TODO: implement preprocessing of PIL image objects
-        elif isinstance(image, SimpleITK.Image):
-            # TODO: implement preprocessing of SimpleITK image objects
+            # cv2 array format: height:width:channels
+            # required input: (826, 551, 3)
+            # input from PIL: (1, 3, 826, 551)
+            # npArr = npArr.reshape(npArr.shape[2], npArr.shape[3], npArr.shape[1])
+            #
+            # npArr = cv2.cvtColor(npArr, cv2.COLOR_RGB2BGR)
+
+            npArr = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+
+            det_threshold = [0.6,0.7,0.8]
+            mtcnn_path = os.path.join(os.path.dirname('__file__'), 'model/mtcnn-model')
+            ctx = mx.cpu()
+            detector = MtcnnDetector(model_folder=mtcnn_path, ctx=ctx, num_worker=1,
+            accurate_landmark = True, threshold=det_threshold)
+            # Pass input images through face detector
+            ret = detector.detect_face(npArr, det_type = 0)
+            if ret is None:
+                raise Exception("No face detected in input image.")
+            bbox, points = ret
+            if bbox.shape[0]==0:
+                raise Exception("No face detected in input image.")
+            bbox = bbox[0,0:4]
+            points = points[0,:].reshape((2,5)).T
+            # Call preprocess() to generate aligned images
+            nimg = self._preprocess(npArr, bbox, points, image_size='112,112')
+            nimg = cv2.cvtColor(nimg, cv2.COLOR_BGR2RGB)
+            aligned = np.transpose(nimg, (2,0,1))
+            return aligned
         else:
             raise IOError("Image Type not supported for preprocessing.")
-        return image
-
 
     def _preprocessAfterConversionToNumpy(self, npArr):
-        # TODO: implement preprocessing of image after it was converted to a numpy array
+        # pass
         return npArr
 
-
     def computeOutput(self, inferenceResults):
-        # TODO: implement postprocessing of inference results
+        result = normalize(inferenceResults).flatten()
         return result
+
+    def _preprocess(self, img, bbox=None, landmark=None, **kwargs):
+        M = None
+        image_size = []
+        str_image_size = kwargs.get('image_size', '')
+        # Assert input shape
+        if len(str_image_size)>0:
+            image_size = [int(x) for x in str_image_size.split(',')]
+            if len(image_size)==1:
+                image_size = [image_size[0], image_size[0]]
+            assert len(image_size)==2
+            assert image_size[0]==112
+            assert image_size[0]==112 or image_size[1]==96
+
+        # Do alignment using landmark points
+        if landmark is not None:
+            assert len(image_size)==2
+            src = np.array([
+              [30.2946, 51.6963],
+              [65.5318, 51.5014],
+              [48.0252, 71.7366],
+              [33.5493, 92.3655],
+              [62.7299, 92.2041] ], dtype=np.float32 )
+            if image_size[1]==112:
+                src[:,0] += 8.0
+            dst = landmark.astype(np.float32)
+            tform = trans.SimilarityTransform()
+            tform.estimate(dst, src)
+            M = tform.params[0:2,:]
+            assert len(image_size)==2
+            warped = cv2.warpAffine(img,M,(image_size[1],image_size[0]),
+            borderValue = 0.0)
+            return warped
+
+        # If no landmark points available, do alignment using bounding box.
+        # If no bounding box available use center crop
+        if M is None:
+            if bbox is None:
+                det = np.zeros(4, dtype=np.int32)
+                det[0] = int(img.shape[1]*0.0625)
+                det[1] = int(img.shape[0]*0.0625)
+                det[2] = img.shape[1] - det[0]
+                det[3] = img.shape[0] - det[1]
+            else:
+                det = bbox
+            margin = kwargs.get('margin', 44)
+            bb = np.zeros(4, dtype=np.int32)
+            bb[0] = np.maximum(det[0]-margin/2, 0)
+            bb[1] = np.maximum(det[1]-margin/2, 0)
+            bb[2] = np.minimum(det[2]+margin/2, img.shape[1])
+            bb[3] = np.minimum(det[3]+margin/2, img.shape[0])
+            ret = img[bb[1]:bb[3],bb[0]:bb[2],:]
+            if len(image_size)>0:
+                ret = cv2.resize(ret, (image_size[1], image_size[0]))
+            return ret
